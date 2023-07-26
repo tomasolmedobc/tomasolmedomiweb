bb7f48ac8b941c785f3c3784b25aa4f3b70623e6
Tomas Olmedo <tomasolmedobc@gmail.com>
2023-07-26 12:46:04 -0300

    Nueva implementación de funciones, queda arreglar el tema que no imprime en pantalla, pero si funciona la ejecución

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,11 +1,56 @@
-
+/*
 function agregarLista() {
     var input = document.getElementById("input"); //objeto
     var lista = document.getElementById("lista"); //objeto
     var tarea = input.value; //String
-    var anterioTarea = lista.innerHTML;
+    var anteriorTarea = lista.innerHTML;
 
-    lista.innerHTML = "<li>" + tarea + "</li>" + anterioTarea;
+    lista.innerHTML = "<li>" + tarea + "</li>" + anteriorTarea;
     input.value = "";
+}
 
+*/
+/* Una forma de agregar elementos al array a traves del input
+
+var tareas = [];
+function agregarLista() {
+    var input = document.getElementById("input");
+    var lista = document.getElementById("lista");
+    var tarea = input.value;
+    tareas.push(tarea);
+    lista.innerHTML = "";
+    for(var i = 0; i < tareas.lenght; i++){
+        lista.innerHTML = lista + "<ul>" + tareas[i] + "</ul>";
+    }
+    console.log(tareas);
+    input.value = "";
+}
+
+*/
+// Modelo de datos
+var tareas = [];
+
+function agregarLista() {
+    var input = document.getElementById("input");
+    var tarea = input.value;
+    tareas.push(tarea);
+    imprimirLista();
+    input.value = "";
+}
+function sacarLista(){
+    tareas.pop();
+    imprimirLista();
+
+}
+function limpiarLista(){
+    tareas = [];
+    imprimirLista();
+}
+
+function imprimirLista() {
+    var lista = document.getElementById("lista");
+    lista.innerHTML = "";
+    for(var i=0; i < tareas.lenght; i++) {
+        lista.innerHTML = lista.innerHTML + "<li>" + tareas[i] + "</li>";
+    }
 }
