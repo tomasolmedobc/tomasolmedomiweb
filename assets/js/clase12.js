69bab660dfd2306d14195248a0a21673d8f0c6c6
Tomas Olmedo <tomasolmedobc@gmail.com>
2023-07-28 21:07:13 -0300

    Se hoy en la clase se vio clases y Jquery

---
+++ assets/js/clase12.js
@@ -0,0 +1,22 @@
+/*
+class Persona{
+    constructor(){
+        this.nombre = "";
+        this.apellido = "";
+        this.edad = "";
+        this.email = "";
+        this.password = "";
+    }
+    saludar (){
+        return this.nombre + " " + this.apellido;
+    }
+}
+
+var persona1 = new Persona('Tomas', 'Olmedo')
+persona1
+
+*/
+// Ejemplo de JSON
+var persona = {
+    "nombre" :
+}
