9aeb70eac866a4e926d2a31aa84d79caa9cd59bb
Tomas Olmedo <tomasolmedobc@gmail.com>
2023-08-16 12:25:43 -0300

    Correccion del merge de testing

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-/*const express = require('express')
-const app = express()
-const port = 3000
-const dirbase = "/assets"
-app.use('/assets', express.static(path.join(__dirname, '/assets')))
-
-app.get('/', (req, res) => {
-    let email = req.query.email;
-    let password = req.query.password;
-    res.sendFile(path.join(__dirname, '/login.html'))
-    //res.send('Hello World!')
-    if (email === "a@a.com" && password === "1"){
-        //email y contrasenia validas
-        res.send({"estado" : "exito"});
-    } else{
-        res.send({"estado" : "invalido"});
-    }
-}) */
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
+
+function logger(req, res, next) {
+    console.log(`Request received`)
+    next()
+}
+app.use(logger);
+app.use(express.static('assets'));
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.resolve(__dirname, 'login.html'))
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'assets/js/panel-de-usuario.js'))
 })
 
 app.listen(port, () => {
